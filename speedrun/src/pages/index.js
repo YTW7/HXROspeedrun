c0fce3b6e672120a04aaee027277a4aaf22424d7
YTW7 <91966435+YTW7@users.noreply.github.com>
2023-05-25 19:00:18 +0000

    ui update

--- speedrun/src/pages/index.js
+++ speedrun/src/pages/index.js
@@ -10,6 +10,29 @@ export default function Home() {
    
    <Homepage/>
 
+   <section className='grid grid-cols-2 '>
+   <div className='bg-emerald-300 h-56 rounded-xl my-5 mx-5 flex items-center justify-center'>
+    <div>
+    <h1 className='text-center text-6xl font-bold mb-3'>Parimutuel SDK</h1> 
+    <p>Learn to build a dual-outcome parimutuel market <br/> for a custom time frame.</p>
+    <button className='bg-emerald-500 px-3 py-3 rounded-full mt-2 font-bold'>
+      Let's Go Buidl!
+    </button>
+    </div>
+   </div>
+
+   <div className='bg-pink-300 h-56 rounded-xl my-5 mx-5 flex items-center justify-center'>
+    <div>
+    <h1 className='text-center text-6xl font-bold mb-3'>Dexterity SDK</h1> 
+    <p>Learn to build a dual-outcome parimutuel market <br/> for a custom time frame.</p>
+    <button className='bg-pink-500 px-3 py-3 rounded-full mt-2 font-bold'>
+      Let's Go Buidl!
+    </button>
+    </div>
+   </div>
+   </section>
+   
+  
    </>
   )
 }
