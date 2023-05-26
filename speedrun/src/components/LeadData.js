52b6f31ba0e7dd65304fe4a4e21a83609ab88a94
extracoolkid <115365708+extracoolkid@users.noreply.github.com>
2023-05-26 17:21:04 +0000

    basic updates

--- speedrun/src/components/LeadData.js
+++ speedrun/src/components/LeadData.js
@@ -18,7 +18,7 @@ export default function LeadData() {
   return (
    <>
       <table className="min-w-full table-auto mt-5 rounded-xl">
-            <thead>
+            {/* <thead>
                 <tr className="bg-gray-800 ">
                     <th className="px-16 py-2">
                         <span className="text-gray-200 rounded-tl-xl">Rank</span>
@@ -34,7 +34,7 @@ export default function LeadData() {
                     </th>
                 </tr>
 
-            </thead>
+            </thead> */}
               <tbody className="bg-gray-200">
               {
                        updatedData.map((obj, i) => <Tr {...obj} key={i} />)
@@ -46,26 +46,47 @@ export default function LeadData() {
   )
 }
 
-function Tr({rank, username, points, publicKey }){
-  return(
-      <tr className="bg-gray-50 text-center">
-      {/* <td className="px-16 py-2 flex flex-row items-center">
-          <img src={avatar || '#'} alt="" />
-          <span className="text-center ml-2 font-semibold">{name || "Unknown"}</span>
-      </td> */}
-      <td className="px-16 py-2">
-          <span>{rank || "Unknown"}</span>
-      </td>
-      <td className="px-16 py-2">
-          <span>{username || "Unknown"}</span>
-      </td>
-      <td className="px-16 py-2">
-          <span>{points || "Unknown"}</span>
-      </td>
-      <td className="px-16 py-2">
-          <button className="cursor"><span className=" px-5 py-1 ">{publicKey || "Unknown"}</span></button>
-      </td>
-  </tr>
+// function Tr({rank, username, points, publicKey }){
+//   return(
+//       <tr className="bg-gray-50 text-center">
+//       {/* <td className="px-16 py-2 flex flex-row items-center">
+//           <img src={avatar || '#'} alt="" />
+//           <span className="text-center ml-2 font-semibold">{name || "Unknown"}</span>
+//       </td> */}
+//       <td className="px-16 py-2">
+//           <span>{rank || "Unknown"}</span>
+//       </td>
+//       <td className="px-16 py-2">
+//           <span>{username || "Unknown"}</span>
+//       </td>
+//       <td className="px-16 py-2">
+//           <span>{points || "Unknown"}</span>
+//       </td>
+//       <td className="px-16 py-2">
+//           <button className="cursor"><span className=" px-5 py-1 ">{publicKey || "Unknown"}</span></button>
+//       </td>
+//   </tr>
 
-  )
-}
+//   )
+// }
+function Tr({rank, username, points, publicKey }){
+    return(
+        <tr className="bg-gray-50 flex bg-black">
+         <div class="flex flex-wrap w-full">
+      <div class="p-2 w-full ">
+        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+        <h2 class="text-gray-900 title-font font-medium mr-2 text-white">Rank {rank}</h2>
+          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80"/>
+          <div class="flex-grow">
+            <h2 class="title-font font-medium text-emerald-300">{username}</h2>
+            <p class="text-pink-300">{points} Points</p>
+            <p class="text-gray-500">{publicKey}</p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </tr>
+  
+    )
+  }
+  
