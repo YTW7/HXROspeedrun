6a44e5d5a46418f35c2b2af60eb9cc189c7ab422
YTW7 <91966435+YTW7@users.noreply.github.com>
2023-06-20 11:12:22 +0000

    changed bas url

--- speedrun/lib/helper.js
+++ speedrun/lib/helper.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://ytw7-cautious-winner-x694v9wqjgfvvgq-3000.preview.app.github.dev/"
+const BASE_URL = "https://hxrospeedrun.vercel.app/"
 
 export const getUsers = async () => {
     const response = await fetch(`${BASE_URL}api/users`)
